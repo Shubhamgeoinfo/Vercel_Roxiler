43728a3d81b4473bc0f18bb9668a764b47771908
Shubhamgeoinfo <shubhamgeoinfo@gmail.com>
2024-03-24 16:30:27 +0530

    adding  hardcoded url vercel config

--- config/db.js
+++ config/db.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log({"conn":process.env.MONGO_URL})
+    const conn = await mongoose.connect(
+      "mongodb+srv://shubhamgeoinfo:QHEWvcyuf1wqfpbb@cluster0.9tw8rlp.mongodb.net/roxiler"
+    );
     console.log(
       `Server is Conneted To Mongodb Databse and ready for CRUD operation ${conn.connection.host}`
     );
   } catch (error) {
-    console.log({"conn":process.env.MONGO_URL})
     console.log(
       `Connection Error found  in db file during connection to Database ${error}`
     );
